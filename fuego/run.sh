9e294cc64f7508e7963fa99a7baa9ccb2ad577fa
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2021-05-10 15:36:47 +0200

    Checkpoint

--- fuego/run.sh
+++ fuego/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 die() { echo "$*" 1>&2 ; exit 1; }
 cd "$(dirname "$0")"
-./run.py `echo $1 | sed 's/[^.]*$//'` || die
+./run.py `echo $1 | sed 's/.ck2$//'` || die
 mkdir -p share/mechanisms
 output=`pwd`/share/mechanisms/`echo $1 | sed 's/[^.]*$/c/'`
 PYTHONPATH=.:fuego:pyre:journal:weaver python2 applications/fmc.py --mechanism=$1 --trans=$1.transport.dat --name=$output &&\
