6cd3a387efca825d303efce54e8f258df88ef25b
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2021-07-20 10:37:53 +0200

    Validate rates

--- validate.py
+++ validate.py
@@ -44,19 +44,6 @@ cantera_transport = conductivity, viscosity, *diffusion
 
 error = lambda a,b: abs(a-b)/abs(b) if min(abs(a),abs(b)) > 0 else 0 #min(abs(a),abs(b))
 
-if True:
-    print('\n'.join([f'{name:6}: {a:.2e} {b:.2e} {abs(a-b):.0e} {error(a,b):.0e}' for (name, a, b) in zip(['λ','μ']+species, nekRK_transport, cantera_transport) if a != 0 and b != 0]))
-else:
-    if True:
-        print(f"{'':6}: {'NekRK':8} {'Cantera':8} abs rel")
-        print('\n'.join([f'{name:6}: {a/1e6:+8.2f} {b/1e6:+8.2f} {abs(a-b)/1e6:.0e} {error(a,b):.0e} ' for (name, a, b) in zip(species, rates, cantera) if a != 0 and b != 0]))
-    else:
-        fuego_species = cantera_species #s['name'] for s in ruamel.yaml.YAML().load(open(yaml))['species'] # FIXME: Assumes Fuego and Cantera use same specie indices
-        env = environ
-        env["LD_LIBRARY_PATH"] = 'build'
-        run = subprocess.run(cwd='fuego', env=env, args=['build/bk1','Serial','1','1','0',argv[1]], capture_output=True);
-        run.check_returncode() #assert len(run.stderr)==0, run.stderr
-        fuego = run.stdout.decode().split()
-        fuego = [float([fuego_species.index(s)]) for s in species]
-        print(f"{'':6}: {'NekRK':8} {'Cantera':8} {'Fuego':8} new abs rel old abs rel [mol/cm³/s])")
-        print('\n'.join([f'{name:6}: {a/1e6:+8.2f} {b/1e6:+8.2f} {c/1e6:+8.2f} {abs(a-b):.0e} {error(a,b):.0e} {abs(c-b):.0e} {error(c,b):.0e}' for (name, a, b, c) in zip(species, rates, cantera, fuego) if a != 0 and b != 0]))
+print(f"{'':6}: {'NekRK':8} {'Cantera':8} abs rel")
+print('\n'.join([f'{name:6}: {a/1e6:+8.2f} {b/1e6:+8.2f} {abs(a-b)/1e6:.0e} {error(a,b):.0e} ' for (name, a, b) in zip(species, rates, cantera) if a != 0 and b != 0]))
+print('\n'.join([f'{name:6}: {a:.2e} {b:.2e} {abs(a-b):.0e} {error(a,b):.0e}' for (name, a, b) in zip(['λ','μ']+species, nekRK_transport, cantera_transport) if a != 0 and b != 0]))
